9dc68ea1d4219881139b2c8b118ccdb21695e325
Iago-Santos-Sousa <iago.santos.sousa@gmail.com>
2023-09-01 11:20:59 -0300

    cm-03

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,83 @@
 import { useState } from "react";
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 const signs = ["%", "/", "X", "-", "+"];
 
 function App() {
-  const [currentNum, setCurrent] = useState("");
-  const [prevNum, setPrev] = useState("");
+  const [currentNum, setCurrentNum] = useState("0");
+  const [prevNum, setPrevNum] = useState("");
   const [operator, setOperator] = useState("");
   const [result, setResult] = useState("");
-  console.log(`numero atual ${currentNum}`);
-  console.log(`numero anterior ${prevNum}`);
-  console.log(`resultado ${result}`);
 
   const handleAddNumber = (ele) => {
-    if (result) {
-      setPrev("");
-      setCurrent("");
-      setResult("");
-      setOperator("");
-    }
-    setCurrent((prev) => prev + ele);
+    setCurrentNum((prev) => {
+      const fullValue = prev + ele;
+      if (currentNum === "0") {
+        return ele;
+      }
+      if (Number(currentNum) % 1 === 0) {
+        return prev + ele;
+      }
+      if (fullValue.includes(",")) {
+        const newValue = fullValue.replace(/,/g, ".");
+        return newValue;
+      }
+      return fullValue;
+    });
+  };
+
+  const handleDeleteNumber = () => {
+    setCurrentNum((prev) => {
+      const deleteNumber = prev.slice(0, -1);
+      return deleteNumber;
+    });
   };
 
   const handleSign = () => {
-    if (currentNum > 0) {
-      setCurrent((prev) => `-${prev}`);
+    if (Number(currentNum) > 0) {
+      setCurrentNum((prev) => `-${prev}`);
     }
   };
 
   const handleClear = () => {
-    setPrev("");
-    setCurrent("");
+    setPrevNum("");
+    setCurrentNum("");
     setResult("");
     setOperator("");
   };
 
   const handleOperator = (ele) => {
     if (result) {
-      setPrev(result);
+      setOperator(ele);
+      setCurrentNum("");
+      setPrevNum(result);
       setResult("");
+      return;
     }
     setOperator(ele);
-    setPrev(currentNum);
-    setCurrent("");
+    setPrevNum(currentNum);
+    setCurrentNum("");
   };
 
   const evaluateHandler = () => {
-    let resultValue = null;
+    let resultValue;
     switch (operator) {
       case "/":
         resultValue = parseFloat(prevNum) / parseFloat(currentNum);
         setResult(resultValue.toString());
+        break;
       case "X":
         resultValue = parseFloat(prevNum) * parseFloat(currentNum);
         setResult(resultValue.toString());
+        break;
       case "-":
         resultValue = parseFloat(prevNum) - parseFloat(currentNum);
         setResult(resultValue.toString());
+        break;
       case "+":
         resultValue = parseFloat(prevNum) + parseFloat(currentNum);
         setResult(resultValue.toString());
+        break;
+
       default:
         return;
     }
@@ -77,6 +96,7 @@ function App() {
       )}
       <button onClick={() => handleClear()}>AC</button>
       <button onClick={() => handleSign()}>+/-</button>
+      <button onClick={() => handleDeleteNumber()}>DEL</button>
 
       {numbers.map((ele, index) => (
         <button key={index} onClick={() => handleAddNumber(ele)}>
